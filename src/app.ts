a918684da05cba9042ea7a893759bd74e3cd995b
Alice Yen <gp40424@gmail.com>
2025-05-07 20:35:14 +0800

    feat: cookie-parser morgan cors credentials + updateDesign patch

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from "express";
+import morgan from "morgan"; // HTTP request logger middleware for node.js
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoutes";
 import materialsRoutes from "./routes/materialsRoutes";
 import designsRoutes from "./routes/designsRoutes";
@@ -7,11 +9,14 @@ import { errorMiddleware } from "./middlewares/errorMiddleware";
 
 const app = express();
 app.use(express.json()); // parse the req body with json data。Express v4.16.0 後，之前會用 body-parser library
+app.use(cookieParser());
 app.use(
   cors({
     origin: "http://localhost:3000",
+    credentials: true,
   })
 );
+app.use(morgan("dev"));
 
 app.use("/api/auth/", authRoutes);
 app.use("/api/materials/", materialsRoutes);
--- src/routes/designsRoutes.ts
+++ src/routes/designsRoutes.ts
@@ -16,7 +16,12 @@ const router = Router();
 router.get("/:designId", validateDesignId, validateDesignExists, getDesign);
 router.get("/user/:userId", getDesignsByUser);
 router.post("/", createDesign);
-router.put("/:designId", validateDesignId, validateDesignExists, updateDesign);
+router.patch(
+  "/:designId",
+  validateDesignId,
+  validateDesignExists,
+  updateDesign
+);
 router.delete(
   "/:designId",
   validateDesignId,
